3c2d97065994fa5f4626276c905df92575ef2fac
Brando Miranda <brandomiranda@dhcp-18-189-24-62.dyn.MIT.EDU>
2015-07-20 16:23:26 -0400

    working on all permutations of an image

--- task4_HBF_parts/v1/build_all_combination_of_synthetic_images.m
+++ task4_HBF_parts/v1/build_all_combination_of_synthetic_images.m
@@ -2,25 +2,15 @@ function [ X ] = build_all_combination_of_synthetic_images( parts,num_parts,D )
 %build_all_combination_of_synthetic_images builds all perm. of P1,...,P4
 %   Forms the artificial data set containing all permutations of artificial
 %   images.
-x = zeros(D*num_parts);
-for i=1:num_parts;
-    pi = parts(i);
-    indices = 1:num_parts;
-    indices = indices(indices~=i);
-    for j=indices;
-        pj = parts(j);
-        indices = indices(indices~=j);
-        for k=indices;
-            pk = parts(k);
-            indices = indices(indices~=k);
-            for l=indices;
-                pl = parts(l);
-                indices = indices(indices~=k);
-            end;
-        end;
+indices = 1:1:num_parts;
+permutations = perms(indices);
+for index_perm=1:permutations;
+    permutation_list = permutations(index_perm);
+    x_p = zeros(D,D,num_parts);
+    for i=permutation_list;
+        part_i = parts(i);
+        x_p(:,:,i) = part_i;
     end;  
 end;
-
-
 end
 
