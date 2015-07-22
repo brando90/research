86feaf91878a2a683bcc2829743ebfefc14b264a
Brando Miranda <brandomiranda@dhcp-18-189-44-216.dyn.mit.edu>
2015-07-22 14:45:48 -0400

    debugging HBF

--- task4_HBF_parts/v1/learn_HBF_parameters.m
+++ task4_HBF_parts/v1/learn_HBF_parameters.m
@@ -6,11 +6,12 @@ function [ C, T ] = learn_HBF_parameters( X_data, Y, C_weights, t_centers )
 [K, ~] = size(t_centers); %K = number of hidden units
 C = zeros(K, D);
 T = zeros(D,K);
-[F_star, ~] = f_star_all_data(x, C_weights, t_centers);
-meu = 0.6;
+[F_star, ~] = f_star_all_data(X_data, C_weights, t_centers);
+meu_c = 0.6;
+meu_t = 0.6;
 for iteration=1:100
-    %update weight c_k,j
-    X_data_t = X_data.';
+    %%update weight c_k,j
+    %X_data_t = X_data.';
     delta = Y - F_star;
     for j=1:K
         for k=1:D
@@ -23,19 +24,30 @@ for iteration=1:100
                 G_xi_tk = exp(-1*norm(x_i - t_k)^2);
                 total_change = total_change + delta_i*(-2)*G_xi_tk;
             end
-            c_kj = c_kj - meu*(total_change);
+            c_kj = c_kj - meu_c*(total_change);
             C(k,j) = c_kj;
         end
     end
-    %update center t_k
+    %%update center t_k
     for k=1:K
-        t_k = t_centers(k);
-        c_k = C_weights(k,:);
-        for i=1:N
-            delta_i = norm(delta(i), 2)^2;
-            
+        t_k = t_centers(:,k);
+        gradient = zeros(size(t_k));
+        for j=1:D
+            c_kj = C_weights(k,j);
+            change = zeros(size(t_k));
+            for i=1:N
+                x_i = X_data(:,i);
+                delta_i = norm( delta(:,i) , 2)^2;
+                g_ik = exp( -1 * norm ( x_i - t_k,2)^2 );
+                change = change + delta_i*g_ik*(x_i - t_k); 
+            end
+            gradient = gradient + 4*c_kj*change;
         end
+        t_k = t_k - meu_t*change;
+        T(:,k) = t_k;
     end
+    C_weights = C;
+    t_centers = T;
 end
 end
 
