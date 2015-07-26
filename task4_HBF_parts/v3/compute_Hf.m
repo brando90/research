6c766d7525f431bd132c308394689ab4b8aa5049
Brando Miranda <brandomiranda@Brandos-MacBook-Pro.local>
2015-07-26 19:57:03 -0400

    everything seems in order

--- task4_HBF_parts/v3/compute_Hf.m
+++ task4_HBF_parts/v3/compute_Hf.m
@@ -1,12 +1,20 @@
-function [ output_args ] = compute_Hf( c, t, lambda )
-%compute_Hf - computes regularized cost H[F] = sum (y_i, f(x_i))^2 +
-%lambda || Pf ||^2
+function [ regularized_cost_hf ] = compute_Hf( X, Y, c, t, lambda )
+%compute_Hf - computes regularized cost 
+%H[F] = sum (y_i, f(x_i))^2 + lambda || Pf ||^2
 %   Inputs:
 %       c - weights (K x 1)
 %       t = centers (D  x K)
 %   Outputs:
-%       
-
-
+%       regularized_cost_hf = cost H[f]=sum(y_i,f(x_i))^2+lambda||Pf||^2
+[~, N] = size(X);
+empirical_risk = 0;
+for i=1:N
+    x_i = X(:, i);
+    f_i = f_start(x_i, c, t);
+    y_i = Y(i);
+    delta_i = y_i - f_i;
+    empirical_risk = empirical_risk + delta_i^2;
 end
-
+regularization_penalty = 0;
+regularized_cost_hf =  empirical_risk + lambda * regularization_penalty;
+end
---
+++ task4_HBF_parts/v3/learn_HBF_parameters_1_hidden_layer.m
@@ -0,0 +1,25 @@
+function [ c_new, t_new ] = learn_HBF_parameters_1_hidden_layer(X,Y,c,t,lambda,mu_c,mu_t, err)
+%learn_HBF_parameters_1_hidden_later - learns HBF params from Poggio's Paper
+%   Inputs:
+%       X = data matrix (D x N)
+%       Y = labels (N x 1)
+%       c = weights (K x 1)
+%       t = centers (D x K)
+%       lambda = regularization param (1 x 1)
+%       mu_c = (1 x 1)
+%       mu_t = (1 x 1)
+%   Outputs:
+%       c_new = learned weights (K x 1)
+%       t_new = learned centers (D x K)
+current_error = compute_Hf(X, Y, c, t, lambda);
+while current_error > err
+    % Compute Gradients
+    dhf_dc = compute_weight_gradient(X, Y, c, t, lambda); % Computes gradient wrt to weights c's
+    dhf_dt = compute_center_gradient(X, Y, c, t, lambda); % Computes gradient wrt to ceters t's
+    % Update parameters
+    c_new = c_new - mu_c * dhf_dc;
+    t_new = t_new - mu_t * dhf_dt;
+    current_error = compute_Hf(X, Y, c_new, t_new, lambda);
+end
+end
+
