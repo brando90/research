02af420915362c1e8e5b2d340153d012b8a720e2
Brando Miranda <brandomiranda@Brandos-MacBook-Pro.local>
2015-08-05 02:42:17 -0400

    in the middle of work

--- task4_HBF_parts/v3/two_2_layer_HBF/update_t1_gradient.m
+++ task4_HBF_parts/v3/two_2_layer_HBF/update_t1_gradient.m
@@ -3,6 +3,7 @@ function [ t1 ] = update_t1_gradient(t1,x,y,f,z_l1,z_l2,a_l2,a_l3,c,t2,lambda,mu
 %   Updates t1 according to:
 %       t1 := t1 - mu_c * dJ/dt1
 %   Input:
+%       x = data (Dp x 1)
 %       y = label (1 x 1)
 %       f = f(x) (1 x 1)
 %       z_l1 = inputs l2 (Np x Dd)
@@ -15,9 +16,12 @@ function [ t1 ] = update_t1_gradient(t1,x,y,f,z_l1,z_l2,a_l2,a_l3,c,t2,lambda,mu
 %       mu_c = step size (1 x 1)
 %   Output:
 %       t1 = updated weights (K2 x 1)
+x_parts = reshape(x, [Dp, Np])'; % Np x Dp
 [Np, Dp] = size(a_l2);
 K1 = Np * Dp;
 a_l2_col_vec = reshape(a_l2, [K1, 1]);
 alpha = bsxfun(@minus, a_l2_col_vec, t2);
 alpha = reshape(alpha, [Np, Dp]);
+xi_t1 = bsxfun(@minus, x_parts, t1);
+permute(bsxfun(@minus, alpha, permute(xi_t1, [1, 3, 2])), [1, 3, 2]);
 end
