a73f814296b25a8d0482b9f8e991dfc75b0ec60f
Brando Miranda <brandomiranda@dhcp-18-189-14-223.dyn.mit.edu>
2015-08-20 13:36:42 -0400

    numerical derivatives or 1HBF

--- task4_HBF_parts/v3/one_1_layer_HBF/compute_df_dt_numerical_derivatives.m
+++ task4_HBF_parts/v3/one_1_layer_HBF/compute_df_dt_numerical_derivatives.m
@@ -1,4 +1,19 @@
-function [ df_dt ] = compute_df_dt_numerical_derivatives(  )
-%compute_df_dc_numerical_derivatives
-
+function [ df_dt ] = compute_df_dt_numerical_derivatives( x, c, t, eps )
+%compute_df_dt_numerical_derivatives
+%       x = data matrix (D x 1)
+%       c = weights (K x 1)
+%       t = centers (D x K)
+%       eps = epsilon (1 x 1)
+[D, K] = size(t);
+df_dt = zeros([D, K]);
+for k=1:K
+    for d=1:D
+        e = zeros([D, K]);
+        e(d,k) = eps;
+        f_e1 = f_star(x, c, t+e);
+        f_e2 = f_star(x, c, t-e);
+        numerical_derivative = (f_e1 - f_e2)/(2*eps);
+        df_dt(d,k) = numerical_derivative;
+    end
+end
 end
