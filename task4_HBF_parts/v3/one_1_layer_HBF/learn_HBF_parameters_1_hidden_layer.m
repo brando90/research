92033fb13468a800223f633ba2514ead9ae6f619
Brando Miranda <brandomiranda@dhcp-18-189-14-223.dyn.MIT.EDU>
2015-08-19 21:44:24 -0400

    re writing 1HBF code

--- task4_HBF_parts/v3/one_1_layer_HBF/learn_HBF_parameters_1_hidden_layer.m
+++ task4_HBF_parts/v3/one_1_layer_HBF/learn_HBF_parameters_1_hidden_layer.m
@@ -1,4 +1,4 @@
-function [ c, t ] = learn_HBF_parameters_1_hidden_layer(X,Y,c,t,lambda,mu_c,mu_t, prec, visualize)
+function [ c, t ] = learn_HBF_parameters_1_hidden_layer(X,y,c,t,lambda,mu_c,mu_t,prec,visualize)
 %learn_HBF_parameters_1_hidden_later - learns HBF params from Poggio's Paper
 %   Inputs:
 %       X = data matrix (D x N)
@@ -13,7 +13,7 @@ function [ c, t ] = learn_HBF_parameters_1_hidden_layer(X,Y,c,t,lambda,mu_c,mu_t
 %   Outputs:
 %       c = learned weights (K x 1)
 %       t = learned centers (D x K)
-current_error = compute_Hf(X, Y, c, t, lambda);
+current_error = compute_Hf(X, y, c, t, lambda);
 prev_error = inf;
 i = 1;
 %print_errors_debug(prev_error,current_error, prec, i);
@@ -22,12 +22,10 @@ errors{1,1} = current_error;
 while abs(current_error - prev_error) > prec
     prev_error = current_error;
     % Compute Gradients
-    dhf_dc = compute_weight_gradient(X, Y, c, t, lambda); % Computes gradient wrt to weights c's
-    dhf_dt = compute_center_gradient(X, Y, c, t, lambda); % Computes gradient wrt to ceters t's
+    c_new = update_t_batch(X, y, c, t, mu_c, lambda); % Computes gradient wrt to weights c's
+    t_new = update_c_batch(X, y, c, t, mu_t, lambda); % Computes gradient wrt to ceters t's
     % Update parameters
-    c = c - mu_c * dhf_dc;
-    t = t - mu_t * dhf_dt;
-    current_error = compute_Hf(X, Y, c, t, lambda);
+    current_error = compute_Hf(X, y, c, t, lambda);
     errors{i} = current_error;
     i = i + 1;
     %print_errors_debug(prev_error,current_error, prec, i);
