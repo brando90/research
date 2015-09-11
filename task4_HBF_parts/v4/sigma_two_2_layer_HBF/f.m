10d04c9aa718835cf7eb9db23a01c702c4e7e793
Brando Miranda <brandomiranda@Brandos-MacBook-Pro.local>
2015-09-10 20:05:34 -0400

    code

--- task4_HBF_parts/v4/sigma_two_2_layer_HBF/f.m
+++ task4_HBF_parts/v4/sigma_two_2_layer_HBF/f.m
@@ -1,24 +1,33 @@
-function [ f_x, z_l1, z_l2, a_l2, a_l3 ] = f( x, c, t1, t2, sig )
+function [ f_x, z_l1, z_l2, a_l2, a_l3 ] = f( x, C, t1, t2, sig )
 %f_start - computes 2 layer HBF predictor
 %   Computes f^*(x) = sum_i c_i a^(3)_i
 %   Inputs:
 %       x = data point (D x 1)
 %           x = [x1, ..., x_np, ..., x_Np]
-%       c = weights (K2 x 1)
+%       C = weights (K2 x L)
 %       t2 = centers (K1 x K2)
 %       t1 = centers (Dp x Dd x Np)
 %   Outputs:
-%       f = f^*(x) = sum_i c_i a^(3)_i
-%       a_l3 = activations l3 (K2 x 1)
-%       z_l2 = inputs l2 (K2 x 1)
-%       a_l2 = activations l2 (Np x Dd)
-%       z_l1 = inputs l1 (Np x Dd)
+%       f = f^*(x) = sum_i c_i a^(3)_i (L x 1)
+%       z_l1 = inputs l1 (Np x Dd x L)
+%       z_l2 = inputs l2 (K2 x L)
+%       a_l2 = activations l2 (Np x Dd x L)
+%       a_l3 = activations l3 (K2 x L)
 
-%[ f, z_l1, z_l2, a_l2, a_l3 ] = f_star_loops( x, c, t1, t2 );
-
-[ f_x, z_l1_p, z_l2_p, a_l2, a_l3 ] = f_loops( x, c, t1, t2, sig );
-z_l1 = z_l1_p;
-z_l2 = z_l2_p;
-
-%[ f, z_l1, z_l2, a_l2, a_l3 ] = f_star_vec( x, c, t1, t2 );
+[~, Dd, Np] = size(t1);
+[K2, L] = size(C);
+f_x = zeros(L,1);
+z_l1 = zeros(Np,Dd,L);
+z_l2 = zeros(K2,L);
+a_l2 = zeros(Np,Dd,L);
+a_l3 = zeros(K2,L);
+for l=1:L
+    c_l = C(:,l);
+    [ f_x_l, z_l1_l, z_l2_l, a_l2_l, a_l3_l ] = f_loops( x, c_l, t1, t2, sig );
+    f_x(l) = f_x_l;
+    z_l1(:,:,l) = z_l1_l;
+    z_l2(:,l) = z_l2_l;
+    a_l2(:,:,l) = a_l2_l;
+    a_l3(:,:,l) = a_l3_l;
+end
 end
--- task4_HBF_parts/v4/sigma_two_2_layer_HBF/learn_HBF_parameters_2_hidden_layer_iterations.m
+++ task4_HBF_parts/v4/sigma_two_2_layer_HBF/learn_HBF_parameters_2_hidden_layer_iterations.m
@@ -1,9 +1,9 @@
-function [ c, t1, t2 ] = learn_HBF_parameters_2_hidden_layer_iterations(X,y, c,t1,t2,sig, mu_c,mu_t1,mu_t2, lambda, visualize, iterations)
+function [ C, t1, t2 ] = learn_HBF_parameters_2_hidden_layer_iterations(X,y, C,t1,t2,sig, mu_c,mu_t1,mu_t2, lambda, visualize, iterations)
 %learn_HBF_parameters_1_hidden_later - learns HBF params for 2 layers
 %   Inputs:
 %       X = data matrix (D x N)
 %       Y = labels (N x 1)
-%       c = weights (K2 x 1)
+%       c = weights (K2 x L)
 %       t2 = centers (K1 x K2)
 %       t1 = centers (Dp x Dd x Np)
 %       lambda = regularization param (1 x 1)
@@ -17,56 +17,30 @@ function [ c, t1, t2 ] = learn_HBF_parameters_2_hidden_layer_iterations(X,y, c,t
 %       t1 = centers (K_1 x K_2)
 %       t2 = centers (D_p x D_d x N_p)
 [~, N] = size(X);
-%[Dp, ~, Np] = size(t1);
-current_error = compute_Hf(X,y, c,t1,t2, sig, lambda);
-%prev_error = inf;
-%i = 1;
+current_error = compute_Hf(X,y, C,t1,t2, sig, lambda);
 errors = cell(1,1);
 errors{1,1} = current_error;
-%while the current error is too large
-[~, Dd, Np] = size(t1);
-K2 = length(c);
-changes_c = zeros(K2, iterations);
-changes_t1 = zeros(Dd*Np, iterations);
-changes_t2 = zeros(K2, iterations);
-dJ_dc_mu_c_iter = zeros(K2, iterations);
-dJ_dt1_mu_t1_iter = zeros(Dd*Np, iterations);
-dJ_dt2_mu_t2_iter = zeros(K2, iterations);
+%[~, Dd, Np] = size(t1);
+%K2 = length(C);
 for i=1:iterations
     %% choose random data point x,y
     i_rand = randi(N);
     x_i = X(:,i_rand);
     y_i = y(i_rand);
     %% get new parameters
-    [f_i, z_l1_p, z_l2_p, a_l2, a_l3] = f(x_i, c,t1,t2,sig);
-    [c_new, dJ_dc] = update_c_gradient(c, f_i,y_i, a_l3, mu_c, lambda);
-    [t1_new, dJ_dt1] = update_t1_gradient(t1, f_i,x_i,y_i, z_l1_p,z_l2_p,a_l2, c,t2,sig, mu_t1, lambda);
-    [t2_new, dJ_dt2] = update_t2_gradient(t2, f_i,y_i, z_l2_p,a_l2, c,sig, mu_t2,lambda);
-    %% get changes for c/iter.
-    change_c_wrt_iteration = c_new - c;
-    changes_c(:, i) = change_c_wrt_iteration;
-    dJ_dc_mu_c_iter(:, i) = -mu_c * dJ_dc;
-    %% get changes for t1s/iter.
-    change_t1_wrt_iteration = get_dt1_dt(t1, t1_new);
-    distances = reshape(change_t1_wrt_iteration, Dd*Np, 1);
-    changes_t1(:, i) = distances;
-    dJ_dt1_col_norms = get_norms_col_dJ_dt1(dJ_dt1);
-    dJ_dt1_col_norms_reshaped = reshape(dJ_dt1_col_norms, Dd*Np, 1);
-    dJ_dt1_mu_t1_iter(:,i) = mu_t1 * dJ_dt1_col_norms_reshaped;
-    %% get changes for t2s/iter.
-    change_t2_wrt_iteration = get_dt2_dt(t2, t2_new );
-    changes_t2(:, i) = change_t2_wrt_iteration;
-    dJ_dt2_col_norms = get_norms_col_dJ_dt2(dJ_dt2);
-    dJ_dt2_mu_t2_iter(:, i) = mu_t2 * dJ_dt2_col_norms;
+    [f_i, z_l1_p, z_l2_p, a_l2, a_l3] = f(x_i, C,t1,t2,sig);
+    [C_new, ~] = update_c_gradient(C, f_i,y_i, a_l3, mu_c, lambda);
+    [t1_new, ~] = update_t1_gradient(t1, f_i,x_i,y_i, z_l1_p,z_l2_p,a_l2, C,t2,sig, mu_t1, lambda);
+    [t2_new, ~] = update_t2_gradient(t2, f_i,y_i, z_l2_p,a_l2, C,sig, mu_t2,lambda);
     %% update c's
-    c = c_new;
+    C = C_new;
     %% Update t1's
     t1 = t1_new;
     %% Update t2's
     t2 = t2_new;
     %% update errors
     %prev_error = current_error;
-    current_error = compute_Hf(X,y, c,t1,t2, sig, lambda);
+    current_error = compute_Hf(X,y, C,t1,t2, sig, lambda);
     errors{i} = current_error;
     %i = i + 1;
 end
@@ -76,44 +50,4 @@ if visualize
     errors = cell2mat(errors);
     iteration_axis = 1:iterations;
     plot(iteration_axis, errors );
-    %% plot changes in param c
-    for k2=1:K2
-        figure        
-        c_changes_i = changes_c(k2,:); % (1 x iterations)
-        subplot(2,1,1)
-        plot(iteration_axis, c_changes_i)
-        title(strcat('c/iter -- ', num2str(k2) ) )
-        
-        dJ_dc_mu_c = dJ_dc_mu_c_iter(k2,:); % (1 x iterations)
-        subplot(2,1,2)
-        plot(iteration_axis,dJ_dc_mu_c)
-        title(strcat('dJ dc -- ', num2str(k2) ) )
-    end
-    %% plot change in param t1
-    for ddnp=1:(Dd*Np)
-        figure
-        t1_changes_ddnp = changes_t1(ddnp,:); % (1 x iterations)
-        subplot(2,1,1)
-        plot(iteration_axis,t1_changes_ddnp)
-        title(strcat('t1/iter -- ', num2str(ddnp) ) )
-        
-        dJ_dt1_mu_t1 = dJ_dt1_mu_t1_iter(ddnp,:);
-        subplot(2,1,2)
-        plot(iteration_axis,dJ_dt1_mu_t1)
-        title(strcat('dJ t1 -- ', num2str(ddnp) ) )
-    end
-    %% plot changes in param t2
-    for k2=1:K2
-        figure
-        t2_changes_k2 = changes_t2(k2,:); % (1 x iterations)
-        subplot(2,1,1)
-        plot(iteration_axis, t2_changes_k2)
-        title(strcat('t2/iter -- ', num2str(k2)))
-        
-        dJ_dt2_mu_t2 = dJ_dt2_mu_t2_iter(k2,:); % (1 x iterations)
-        subplot(2,1,2)
-        plot(iteration_axis, dJ_dt2_mu_t2)
-        title(strcat('dJ dt2 -- ', num2str(k2))) 
-    end
-end
 end
