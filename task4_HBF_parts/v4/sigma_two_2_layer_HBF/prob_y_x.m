a00464547b3cf5b67b352c9dcc2ad1642eaa4323
Brando Miranda <brandomiranda@Brandos-MacBook-Pro.local>
2015-10-11 19:49:10 -0400

    Z error problem coded...does it work?

--- task4_HBF_parts/v4/sigma_two_2_layer_HBF/prob_y_x.m
+++ task4_HBF_parts/v4/sigma_two_2_layer_HBF/prob_y_x.m
@@ -5,25 +5,18 @@ function [ prob ] = prob_y_x( h_x )
 %   Output:
 %       prob = prob(y=l | x ; theta) (L x 1)
 % prob = h/Z;
-largest = max(h_x); 
-smallest = min(h_x);
-range = largest - smallest;
+%% constants
 lower_limit = ceil( log(realmin('double')) ); %  about -708.3964
 upper_limit = floor( log( realmax('double')) ); % 709.7827
-limit = upper_limit - lower_limit;
-if abs(range) < limit
-    disp('abs(range) < limit')
-    translation = largest - upper_limit
-    h_x_p = h_x - translation
-else
-    disp('else')
-    indices_upper = h_x > upper_limit;
+L = Length(h_x);
+%% Algorithm
+largest = max(h_x);
+if largest > upper_limit - log(L)
+    h_x = h_x - largest - log(L);
     indices_lower = h_x < lower_limit;
-    h_x(indices_upper) = upper_limit;
-    h_x(indices_lower) = lower_limit;  
-    h_x_p = h_x;
+    h_x(indices_lower) = lower_limit;
 end
-exp_h_x = exp(h_x_p);
+exp_h_x = exp(h_x);
 z = sum( exp_h_x );
 prob = exp_h_x/z;
 end
