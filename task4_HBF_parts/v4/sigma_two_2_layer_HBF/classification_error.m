45d37f70b334c1b0eaf633b0dcf016ac2ec01c07
Brando Miranda <brandomiranda@dhcp-18-189-5-212.dyn.MIT.EDU>
2015-10-12 15:49:34 -0400

    prediction and classification error code working

--- task4_HBF_parts/v4/sigma_two_2_layer_HBF/classification_error.m
+++ task4_HBF_parts/v4/sigma_two_2_layer_HBF/classification_error.m
@@ -1,4 +1,4 @@
-function [ total_number_errors ] = classification_error( y, y_predictions )
+function [ total_number_errors, percentage_error ] = classification_error( y, y_predictions )
 %computes classification error
 %   Inputs:
 %       y = true/data labels (N x 1)
@@ -6,5 +6,7 @@ function [ total_number_errors ] = classification_error( y, y_predictions )
 %   Outputs:
 %       total_number_errors = classification error (1 x 1)
 %% number of times y is not equal to y_pred
+N = length(y);
 total_number_errors = sum(y ~= y_predictions); 
+percentage_error = total_number_errors/N;
 end
