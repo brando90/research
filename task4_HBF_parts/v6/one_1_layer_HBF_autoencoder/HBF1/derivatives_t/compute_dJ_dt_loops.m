f20fad4b19076a17ce5c3869cde01828824dd298
Brando Miranda <brandomiranda@dhcp-18-189-38-88.dyn.mit.edu>
2015-10-28 15:33:02 -0400

    dj_dt matches numerical derivatives

--- task4_HBF_parts/v6/one_1_layer_HBF_autoencoder/HBF1/derivatives_t/compute_dJ_dt_loops.m
+++ task4_HBF_parts/v6/one_1_layer_HBF_autoencoder/HBF1/derivatives_t/compute_dJ_dt_loops.m
@@ -11,11 +11,13 @@ function [ dJ_dt ] = compute_dJ_dt_loops(f_x,z,x,y,t,c)
 [D,K] = size(t);
 dJ_dt = zeros(D,K);
 for k=1:K
-   for d=1:D
+    dJ_dt_k = zeros(D,1);
+    for d=1:D
         c_d = c(:,d); %(K x 1)
         dJ_df_d = 2*(y(d) - f_x(d)); % (1 x 1)
-        df_dt = c_d(k) * exp(-z(k)) * 2 * ( x - t(:,k) ); %(D x 1)
-        dJ_dt(:,k) = dJ_df_d * df_dt(d);
-    end 
+        df_dt = -c_d(k) * exp(-z(k)) * 2 * ( x - t(:,k) ); %(D x 1)
+        dJ_dt_k = dJ_dt_k + dJ_df_d * df_dt;
+    end
+    dJ_dt(:,k) = dJ_dt_k;
 end
 end
