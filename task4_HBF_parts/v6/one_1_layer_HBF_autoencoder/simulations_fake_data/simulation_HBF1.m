4afec6c5bb82966daa5a1140f5c50dadc02a4537
Brando Miranda <brandomiranda@dhcp-18-189-104-67.dyn.MIT.EDU>
2015-10-29 11:09:16 -0400

    saving

--- task4_HBF_parts/v6/one_1_layer_HBF_autoencoder/simulations_fake_data/simulation_HBF1.m
+++ task4_HBF_parts/v6/one_1_layer_HBF_autoencoder/simulations_fake_data/simulation_HBF1.m
@@ -16,33 +16,42 @@ X_test_data = normc(X_test_data);
 [Dp, Dd, Np] = size(list_dict)
 D = Dp*Np
 %K = Dd^Np
-K = D
+K = 50
 %% parameter initilization ------------------------------------------------
-%% Random initilization
-% c_initial = rand(K,L);
-% t_initial = rand(D,K);
-% beta = 1;
-%% Random Data points
 %t_initial = datasample(X_training_data', K, 'Replace', false)';
 t_initial = [datasample(X_training_data', K-2, 'Replace', false)', X_perfect_data];
 %t_initial = X_perfect_data;
 %t_initial = X_training_data(:,[1,1002]);
+%t_initial = rand(D,K);
 size(t_initial)
 c_initial = rand(K,D);
 beta = 1;
 %% GD parameters
-mu_c = 0.000001;
-mu_t = 0.0000005;
+mu_c = 0.00003;
+mu_t = 0.000007;
+%mu_c = 0.000009;
+%mu_t = 0.000001;
+% mu_c = 0.005;
+% mu_t = 0.009;
+%mu_c = 0.000005;
+%mu_t = 0;
 lambda = 0; %reg param
 %% Learn the parameters
 disp('============++++++++++++++>>>> TRAINING STARTING');
-iterations = 1
+iterations = 200
 visualize = 1;
 mdl_initial = HBF1(c_initial,t_initial,beta);
 tic
 mdl_final = learn_HBF1_batch_GD(X_training_data,X_training_data, mdl_initial, mu_c,mu_t, lambda, iterations,visualize);
 %mdl_final = learn_HBF1_alternating_minimization(X_training_data,y_training_data, mdl_initial, mu_c,mu_t, lambda, iterations,visualize);
 elapsed_time = toc;
+
+%%
+error_training_initial_model = compute_Hf_sq_error(X_training_data,X_training_data, mdl_initial, lambda)
+error_test_initial_model = compute_Hf_sq_error(X_test_data,X_test_data, mdl_initial, lambda)
+
+error_training_final_model = compute_Hf_sq_error(X_training_data,X_training_data, mdl_final, lambda)
+error_test_final_model = compute_Hf_sq_error(X_test_data,X_test_data, mdl_final, lambda)
 
 %% Time Elapsed
 disp('--==>>iterations')
