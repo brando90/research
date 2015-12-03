a1438c60cb9c98f87a586daa1175fa9c13b375df
Brando Miranda <brandomiranda@dhcp-18-189-66-120.dyn.mit.edu>
2015-12-03 18:51:10 -0500

    MNIST trying to do CV

--- task4_HBF_parts/v7/RBF_multivariant_regression/simulations_cv/simulations_cv_LA.m
+++ task4_HBF_parts/v7/RBF_multivariant_regression/simulations_cv/simulations_cv_LA.m
@@ -13,9 +13,9 @@ addpath('../../common/kernel_functions')
 addpath('../../common/data_generation/simple_regression_example_high_dimensions')
 %% data set
 snr = 10;
-D_out = 1;
 N = 1000;
-D = 20;
+D = 784
+D_out = D
 [X, y] = generate_high_dim_regression( N, D, D_out, snr);
 %% data set split
 per_train = 0.6;
@@ -24,28 +24,29 @@ data_for_cross_validation = cross_validation_data(X,y,per_train,per_cv);
 %%
 beta_start = 0.1;
 beta_end = 10;
-num_betas = 100;
-betas = linspace(beta_start, beta_end, num_betas);
+num_betas = 40;
+betas = linspace(beta_start, beta_end, num_betas)
 %%
 beta = inf;
 mdl_func = @RBF;
 param4mdl_func = @RBF_parameters;
 train_func = @learn_RBF_linear_algebra;
 gd_iterations = -1; %GD
-num_inits = 10;
+num_inits = 1;
 lambda = 0;
 params4mdl_iter = RBF_iterator4training(beta, mdl_func,param4mdl_func,train_func,gd_iterations,num_inits,lambda);
 params4mdl_iter.create_initiliazations(data_for_cross_validation.X_train,D_out);
 %%
-visualize = 1;
+visualize = 0;
 tic
 [ best_mdl, error_best_mdl] = hold_out_cross_validation_with_test_data(data_for_cross_validation, betas, params4mdl_iter, visualize);
 time_passed = toc;
 time_elapsed(-1, time_passed )
-error_best_mdl
-best_beta = best_mdl.beta
+%error_best_mdl
+%best_beta = best_mdl.beta
 %% Plot function
 %Y_pred = best_mdl.predict_data_set(X);
 %figure;
 %plot(X, Y_pred, '-ro',X, y, '-b*')
 %legend('prediction','truth');
+beep;
--- task4_HBF_parts/v7/common/cross_validation/standard_train_cv_test_validation/cross_validation_data.m
+++ task4_HBF_parts/v7/common/cross_validation/standard_train_cv_test_validation/cross_validation_data.m
@@ -47,6 +47,17 @@ classdef cross_validation_data < handle
             X_new = X(:, ordering_x);
             y_new = y(:, ordering_y);
         end
+        % Make a copy of a handle object.
+        function new = copy(this)
+            % Instantiate new object of the same class.
+            new = feval(class(this));
+ 
+            % Copy all non-hidden properties.
+            p = properties(this);
+            for i = 1:length(p)
+                new.(p{i}) = this.(p{i});
+            end
+        end
     end
     
 end
