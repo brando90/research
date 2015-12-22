9f73f0a470dcca2dd37d945cbd470c76f681f599
Brando Miranda <brandomiranda@Brandos-MacBook-Pro.local>
2015-12-22 02:03:55 -0500

    dhfdc works

--- task4_HBF_parts/v7/HBF1_multivariant_regression/HBF1/derivatives_t/compute_dHf_dt_numerical_derivatives.m
+++ task4_HBF_parts/v7/HBF1_multivariant_regression/HBF1/derivatives_t/compute_dHf_dt_numerical_derivatives.m
@@ -15,8 +15,8 @@ for d=1:D
     for k=1:K
         e = zeros([D,K]);
         e(d,k) = eps;
-        Hf_e1 = compute_Hf_sq_error(X,y, HBF1(c,t+e,beta), lambda );
-        Hf_e2 = compute_Hf_sq_error(X,y, HBF1(c,t-e,beta), lambda );
+        Hf_e1 = compute_Hf_sq_error(X,y, HBF1(HBF1_parameters(c,t+e,beta,lambda)), lambda );
+        Hf_e2 = compute_Hf_sq_error(X,y, HBF1(HBF1_parameters(c,t-e,beta,lambda)), lambda );
         numerical_derivative = (Hf_e1 - Hf_e2)/(2*eps);
         dHf_dt_numerical(d,k) = numerical_derivative;
     end
--- task4_HBF_parts/v7/HBF1_multivariant_regression/HBF1/derivatives_t/unit_tests/dHf_dt_unit_test.m
+++ task4_HBF_parts/v7/HBF1_multivariant_regression/HBF1/derivatives_t/unit_tests/dHf_dt_unit_test.m
@@ -1,3 +1,4 @@
+%% dHf_dt_unit_test
 restoredefaultpath;
 addpath('../..');
 addpath('../../derivatives_c');
@@ -6,6 +7,7 @@ addpath('../../update_rules_GD');
 addpath('../../model_functions');
 addpath('../../analytic_tools_analysis_HBF1_GD');
 addpath('../../../../common/squared_error_risk');
+addpath('../../../../common/kernel_functions');
 %% dHf_dt_unit_test
 K = 4;
 D = 3;
@@ -16,13 +18,19 @@ y = rand(D,N);
 c = rand(K,D);
 t = rand(D,K);
 beta = 1;
+mdl_params = HBF1_parameters(c,t,beta,lambda);
 %% reg params
 lambda = 0;
 mu_c = 1;
+%%
+Kern = produce_kernel_matrix( X, t, beta );
+F = Kern * mdl_params.c;
 %% run unit test
-dHf_dt = compute_dHf_dt(X,y, c, t, beta, lambda);
+dHf_dt_loops = compute_dHf_dt_loops(X,y, c, t, beta, lambda);
 eps = 1e-10;
 dHf_dt_numerical = compute_dHf_dt_numerical_derivatives(X,y,c,t,beta, lambda, eps);
+dHf_dt_vec = compute_dHf_dt_vec(F,Kern, X,y, mdl_params );
 %% print derivatives
-dHf_dt
+dHf_dt_loops
 dHf_dt_numerical
+dHf_dt_vec
