9c89bd4f382e32dd0d6e25903804a95507e741d9
Brando <brando90@mit.edu>
2016-01-29 11:19:19 -0500

    everything in

--- script_running_test/simulation_test_dir/dispatcher.py
+++ script_running_test/simulation_test_dir/dispatcher.py
@@ -1,5 +1,8 @@
 #!/home/brando90/envs/phy/bin/python
 import sh as sh
 
+
 for i in range(1,5):
-	sh.Command('sbatch hello_world_worker.sh '+str(i))
+	sh.sbatch('worker.sh' , str(i))
+
+print(sh.echo('done with dispatcher.py script'))
