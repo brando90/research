9c88684eba302738208be3a9d177c1a956f20aa4
Brando <brando90@mit.edu>
2016-01-30 14:48:45 -0500

    skeleton code for dispatcher seems ready, about to test it

--- script_running_test/simulation_test_dir/dispatcher.py
+++ script_running_test/simulation_test_dir/dispatcher.py
@@ -1,19 +1,25 @@
 #!/home/brando90/envs/phy/bin/python
 import sh as sh
+import numpy as np
+import time
+#time.sleep(5)
 
-#processes = []
-#for i in range(1,5):
-	#ps = sbatch('worker.sh' , str(i), _bg=True )
-	#processes.append(ps)
-print(">>> Dispatcher about to run---")
+print("running dispatcher")
+
+start_centers = 5
+end_centers = 200
+num_centers = 10
+centers = np.floor( np.linspace(start_centers, end_centers, num_centers) )
+for center in centers:
+	sbatch('worker.sh' , str(i), _bg=True ) #run as background process == True
 
 f = open('counter.txt', 'w+')
-sh.wc( sh.ls(".", "-1"), "-l",_out="counter.txt")
+sh.wc( sh.ls("./simulation_results", "-1"), "-l",_out="counter.txt")
 file_counter = int(f.readlines()[0])
-print(file_counter)
-print( type(file_counter) )
 
+while file_counter != num_centers
+	time.sleep(1.1) #sleeps in seconds
+	sh.wc( sh.ls("./simulation_results", "-1"), "-l",_out="counter.txt")
+	file_counter = int(f.readlines()[0])	 
 
-#for ps in processes:
-#	ps.wait()
-#sh.rm('./slurm*')
+f.close()
