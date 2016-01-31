2a5c22df6e59b502b153eca11a27e542ecf27e02
Brando <brando90@mit.edu>
2016-01-30 20:01:33 -0500

    current dispatcher

--- script_running_test/simulation_test_dir/dispatcher.py
+++ script_running_test/simulation_test_dir/dispatcher.py
@@ -6,22 +6,24 @@ import timeit
 print("running dispatcher")
 start_time = timeit.default_timer()
 #
-start_centers = 5
-end_centers = 700
-num_centers = 100
+(start_centers, end_centers, num_centers) = (5,700,10)
 centers = np.floor( np.linspace(start_centers, end_centers, num_centers) )
 for center in centers:
 	sh.sbatch('worker.sh' , str(center), _bg=True ) #run as background process == True
-
 f = open('counter.txt', 'w+')
 sh.wc( sh.ls("./simulation_results", "-1"), "-l",_out="counter.txt")
 file_counter = int(f.readlines()[0])
-while file_counter != num_centers:
+while file_counter < num_centers:
 	#time.sleep(0.0001) #sleeps in seconds
 	sh.wc( sh.ls("./simulation_results", "-1"), "-l",_out="counter.txt")
 	f.seek(0)
 	file_counter = int(f.readlines()[0])	 
 elapsed = timeit.default_timer() - start_time
 print('Elapsed time for disptcher: ', elapsed)
+time.sleep(6)
+print(sh.ls("."))
+print(sh.pwd())
+sh.rm(sh.glob("./slurm*"))
+
 
 f.close()
