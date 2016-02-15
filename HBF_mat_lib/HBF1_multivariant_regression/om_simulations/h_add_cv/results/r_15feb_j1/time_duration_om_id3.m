0c061bb8dfd79bdc8da6dd30c5f3afa760c766e2
Brando <brando90@mit.edu>
2016-02-15 11:43:27 -0500

    new results

---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_add_cv/results/r_15feb_j1/time_duration_om_id3.m
@@ -0,0 +1,7 @@
+task_id=3;
+secs=3.054463e+04;
+minutes=5.090772e+02;
+hours=8.484620e+00;
+iterations=42000;
+center=120;
+data_set= '../../../common/data/data_h_add_0.7_0.15_0.15_28000_6000_6000.mat' ;
