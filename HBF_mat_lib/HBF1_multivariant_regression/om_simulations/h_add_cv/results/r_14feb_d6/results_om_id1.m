8abf7e361b5f434129d97e44ce95aab38f0d44ae
Brando <brandojazz@gmail.com>
2016-02-14 19:53:50 -0500

    henry the cat is annoying

---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_add_cv/results/r_14feb_d6/results_om_id1.m
@@ -0,0 +1,6 @@
+task_id=1;
+center=10;
+test_error_HBF1=3.304109e+03;
+train_error_HBF1=3.227307e+03;
+test_error_RBF=4.602895e+03;
+train_error_RBF=4.505777e+03;
---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_add_cv/results/r_14feb_d6/time_duration_om_id1.m
@@ -0,0 +1,7 @@
+task_id=1;
+secs=8.549295e+00;
+minutes=1.424883e-01;
+hours=2.374804e-03;
+iterations=2200;
+center=10;
+data_set= '../../../common/data/data_h_add_0.7_0.15_0.15_28000_6000_6000.mat' ;
