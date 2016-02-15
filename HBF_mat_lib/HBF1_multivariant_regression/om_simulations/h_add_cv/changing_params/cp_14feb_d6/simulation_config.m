5ad2bf2a8de02e3bdacd4b2db7caed8975369c2e
Brando <brandojazz@gmail.com>
2016-02-14 19:53:12 -0500

    debug files

---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_add_cv/changing_params/cp_14feb_d6/simulation_config.m
@@ -0,0 +1,23 @@
+gau_precision = 0.5;
+num_inits = 1;
+iterations = 2200; 
+train_func = @learn_HBF1_SGD;
+mdl_func = @HBF1;
+%train_func = @learn_RBF_SGD;
+%mdl_func = @RBF;
+lambda = 0;
+eta_c = 0.9
+eta_t = 0.9
+visualize = 1
+sgd_errors = 1;
+%% locations
+cp_folder = 'cp_14feb_d6/'
+cp_param_files_names = 'cp_14feb_d6_%d.m'
+results_path = './results/r_14feb_d6/'
+%% jobs
+jobs = 1;
+start_centers = 10;
+end_centers = 10;
+%%
+data_set_path = '../../../common/data/data_h_add_0.7_0.15_0.15_28000_6000_6000.mat';
+data_normalized = 0
