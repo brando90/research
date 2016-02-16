5afe7771f4d8072d9994afd69604d8b3630cb1df
Brando <brandojazz@gmail.com>
2016-02-16 18:09:55 -0500

    gabor funciton being commited

---
+++ HBF_mat_lib/common/data_generation/synthetic_data_2D/h_gabor_visualization.m
@@ -0,0 +1,27 @@
+%% create data from range
+start_val = -2.5;
+end_val = 2.5;
+sqrtN = 200; %N = sqrtN*sqrtN
+N = sqrtN*sqrtN
+x_range = linspace(start_val, end_val, sqrtN);
+y_range = linspace(start_val, end_val, sqrtN);
+%% make meshgrid
+[X,Y] = meshgrid(x_range, y_range);
+%Z = cos(2*pi*(X+Y));
+Z = exp( -(X.^2+Y.^2) ).*cos(2*pi*(X+Y)); %% h_gabour
+figure
+surf(X,Y,Z);
+%% create data set for training alg 
+[ X_data,Y_data] = make_data_from_meshgrid( X,Y,Z );
+[D, N] = size(X_data);
+perm = randperm(N);
+X_data = X_data(:, perm);
+Y_data = Y_data(:,perm);
+% figure
+% scatter3(X_data(1,:),X_data(2,:),Y_data)
+%X_data(:, perm) = X_data;
+%Y_data(:,perm) = Y_data;
+%% make the data back to mesh from training data
+% [Xp,Yp,Zp] = make_meshgrid_data_from_training_data( X_data,Y_data );
+% figure
+% surf(Xp,Yp,Zp);
