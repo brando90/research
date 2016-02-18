76189f4cab7f3d269fc1b797b7b481defb6f7cba
Brando <brandojazz@gmail.com>
2016-02-18 15:32:51 -0500

    sinxy new data set to try

---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_sinxy_cv/results/r_18feb_d1/results_om_id1.m
@@ -0,0 +1,6 @@
+task_id=1;
+center=140;
+test_error_HBF1=5.265469e+02;
+train_error_HBF1=5.255804e+02;
+test_error_RBF=2.638443e-04;
+train_error_RBF=2.536761e-04;
---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_sinxy_cv/results/r_18feb_d1/time_duration_om_id1.m
@@ -0,0 +1,7 @@
+task_id=1;
+secs=6.134490e+01;
+minutes=1.022415e+00;
+hours=1.704025e-02;
+iterations=1110;
+center=140;
+data_set= '../../../common/data/data_h_sinxy_0.7_0.15_0.15_28000_6000_6000.mat' ;
