99ffe72a190762e5872b3aeae38fadc54242629b
Brando <brandojazz@gmail.com>
2016-02-18 16:44:07 -0500

    stuff

--- HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_1_x2_y2_sinxy_cv/results/r_18feb_d1/results_om_id1.m
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_1_x2_y2_sinxy_cv/results/r_18feb_d1/results_om_id1.m
@@ -1,6 +1,6 @@
 task_id=1;
 center=140;
-test_error_HBF1=1.249522e+02;
-train_error_HBF1=1.247367e+02;
-test_error_RBF=1.083485e-03;
-train_error_RBF=1.236197e-03;
+test_error_HBF1=1.020154e+02;
+train_error_HBF1=1.026813e+02;
+test_error_RBF=1.116758e-03;
+train_error_RBF=1.262059e-03;
--- HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_1_x2_y2_sinxy_cv/results/r_18feb_d1/time_duration_om_id1.m
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_1_x2_y2_sinxy_cv/results/r_18feb_d1/time_duration_om_id1.m
@@ -1,7 +1,7 @@
 task_id=1;
-secs=6.470411e+01;
-minutes=1.078402e+00;
-hours=1.797336e-02;
-iterations=1110;
+secs=2.734015e+02;
+minutes=4.556692e+00;
+hours=7.594486e-02;
+iterations=4510;
 center=140;
 data_set= '../../../common/data/data_h_1_x2_y2_sinxy_0.7_0.15_0.15_28000_6000_6000.mat' ;
