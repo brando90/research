332c0f679cf7d8e9ff5514884a7ddc0b15f4cdf7
Brando <brandojazz@gmail.com>
2016-02-20 20:53:10 -0500

    MNIST cv 2

---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/MNIST_cv2/results/r_20feb_d1/results_om_id1.m
@@ -0,0 +1,6 @@
+task_id=1;
+center=10;
+test_error_HBF1=8.982456e+01;
+train_error_HBF1=9.053361e+01;
+test_error_RBF=8.902349e+01;
+train_error_RBF=8.797185e+01;
---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/MNIST_cv2/results/r_20feb_d1/time_duration_om_id1.m
@@ -0,0 +1,7 @@
+task_id=1;
+secs=6.459811e+01;
+minutes=1.076635e+00;
+hours=1.794392e-02;
+iterations=50;
+center=10;
+data_set= '../../../common/data/data_MNIST_0.7_0.15_0.15_49000_10500_10500.mat' ;
