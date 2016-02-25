0147dedc7c9b03275c757b78a5802068b3ab3a7f
Brando <brando90@mit.edu>
2016-02-25 15:09:43 -0500

    results fir HD 10

---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_D10_xsinx2_cv/results/r_21feb_j1/time_duration_om_id1.m
@@ -0,0 +1,7 @@
+task_id=1;
+secs=6.306045e+02;
+minutes=1.051008e+01;
+hours=1.751679e-01;
+iterations=61600;
+center=1;
+data_set= '../../../common/data/data_h_D10_x2sinx_0.7_0.15_0.15_28000_6000_6000.mat' ;
