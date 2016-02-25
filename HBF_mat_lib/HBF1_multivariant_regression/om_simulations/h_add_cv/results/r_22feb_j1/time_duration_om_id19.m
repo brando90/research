f4c3fa0778c9d61a286ccec27913f946f1c8e305
Brando <brando90@mit.edu>
2016-02-25 15:19:03 -0500

    h_add replica

---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_add_cv/results/r_22feb_j1/time_duration_om_id19.m
@@ -0,0 +1,7 @@
+task_id=19;
+secs=3.541478e+04;
+minutes=5.902464e+02;
+hours=9.837440e+00;
+iterations=42000;
+center=170;
+data_set= '../../../common/data/data_h_add_0.7_0.15_0.15_28000_6000_6000.mat' ;
---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_add_cv/results/r_22feb_j1/time_duration_om_id20.m
@@ -0,0 +1,7 @@
+task_id=20;
+secs=5.223714e+04;
+minutes=8.706189e+02;
+hours=1.451032e+01;
+iterations=42000;
+center=180;
+data_set= '../../../common/data/data_h_add_0.7_0.15_0.15_28000_6000_6000.mat' ;
