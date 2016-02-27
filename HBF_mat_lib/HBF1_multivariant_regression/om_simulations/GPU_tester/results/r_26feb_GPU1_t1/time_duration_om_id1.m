1cd3885a1fc5603691745b22caa6b6be4950aa25
Brando <brando90@mit.edu>
2016-02-27 10:50:47 -0500

    gpu results

--- HBF_mat_lib/HBF1_multivariant_regression/om_simulations/GPU_tester/results/r_26feb_GPU1_t1/time_duration_om_id1.m
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/GPU_tester/results/r_26feb_GPU1_t1/time_duration_om_id1.m
@@ -1,7 +1,7 @@
 task_id=1;
-secs=2.019023e+01;
-minutes=3.365038e-01;
-hours=5.608397e-03;
-iterations=2000;
-center=10;
+secs=9.249067e+02;
+minutes=1.541511e+01;
+hours=2.569185e-01;
+iterations=42000;
+center=1;
 data_set= '../../../common/data/data_h_add_0.7_0.15_0.15_28000_6000_6000.mat' ;
---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/GPU_tester/results/r_26feb_GPU1_t1/time_duration_om_id11.m
@@ -0,0 +1,7 @@
+task_id=11;
+secs=3.797452e+03;
+minutes=6.329087e+01;
+hours=1.054848e+00;
+iterations=42000;
+center=95;
+data_set= '../../../common/data/data_h_add_0.7_0.15_0.15_28000_6000_6000.mat' ;
