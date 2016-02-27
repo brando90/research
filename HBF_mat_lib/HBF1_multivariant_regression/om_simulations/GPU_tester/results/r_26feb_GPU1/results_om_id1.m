dd6d9f975329ee7705b7fc46c2a09d0f45849678
Brando <brandojazz@gmail.com>
2016-02-26 19:41:41 -0500

    working on GPU

---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/GPU_tester/results/r_26feb_GPU1/results_om_id1.m
@@ -0,0 +1,6 @@
+task_id=1;
+center=10;
+test_error_HBF1=3.291637e+03;
+train_error_HBF1=3.221936e+03;
+test_error_RBF=2.326644e+03;
+train_error_RBF=2.299642e+03;
---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/GPU_tester/results/r_26feb_GPU1/time_duration_om_id1.m
@@ -0,0 +1,7 @@
+task_id=1;
+secs=7.899416e+00;
+minutes=1.316569e-01;
+hours=2.194282e-03;
+iterations=2000;
+center=10;
+data_set= '../../../common/data/data_h_add_0.7_0.15_0.15_28000_6000_6000.mat' ;
