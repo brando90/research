9abe86b799210eb266a5191bc1ce96d75954d486
Brando <brandojazz@gmail.com>
2016-02-28 16:56:54 -0500

    time plotter results

--- HBF_mat_lib/HBF1_multivariant_regression/om_simulations/GPU_tester/plot_results/plot_errors_vs_iterations_single_model.m
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/GPU_tester/plot_results/plot_errors_vs_iterations_single_model.m
@@ -4,10 +4,10 @@
 % errors_test = best_test;
 % plot_error_vs_iterations_single_model( center, iterations, errors_train, errors_test, eta_c, eta_t)
 %%
-jobs = 4;
+jobs = 20;
 for job_num=1:jobs
     prefix_name = sprintf('test_error_vs_iterations%d',job_num);
-    errors_location = sprintf('../results/r_15feb_j1/%s', prefix_name);
+    errors_location = sprintf('../results/r_26feb_GPU1_t1/%s', prefix_name);
     load(errors_location);
     errors_train = best_train;
     errors_test = best_test;
