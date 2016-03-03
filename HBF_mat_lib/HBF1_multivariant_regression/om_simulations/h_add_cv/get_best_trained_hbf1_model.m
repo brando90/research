f268164de15e220e316f5ad5916085f2ff36e4d0
Brando <brandojazz@gmail.com>
2016-03-03 18:58:53 -0500

    get train

--- HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_add_cv/get_best_trained_hbf1_model.m
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/h_add_cv/get_best_trained_hbf1_model.m
@@ -1,6 +1,4 @@
 function [] = get_best_trained_hbf1_model(slurm_job_id, task_id)
-% gets the best HBF1 model with k centers from the number of initilizations
-%% Load paths
 restoredefaultpath
 slurm_job_id
 task_id
@@ -23,48 +21,104 @@ end
 center
 %% Run Hold Out Cross Validation
 tic;
-error_best_mdl_on_cv = inf;
-best_iteration_mdl = -1;
-%train_func = str2func(train_func_name)
-mdl_func = str2func(mdl_func_name)
-for initialization_index=1:num_inits
-    fprintf('initialization_index = %d\n\n', initialization_index);
-    K = center;
-    %c_init = normc(rand(K,D_out)); % (N x D)
-    c_init = (6 + 6)*rand(K,D_out) - 6;
-    t_init = datasample(X_train', K, 'Replace', false)'; % (D x N)
-    if strcmp( train_func_name, 'learn_HBF1_SGD')
-        mdl_params = HBF1_parameters(c_init,t_init,gau_precision,lambda);
-        [ mdl_params, errors_train, errors_test ] = learn_HBF1_SGD( X_train, y_train, mdl_params, iterations,visualize, X_test,y_test, eta_c,eta_t, sgd_errors);
-    elseif strcmp( train_func_name, 'learn_RBF_SGD')
-        mdl_params = RBF_parameters(c_init,t_init,gau_precision,lambda);
-        [ mdl_params, errors_train, errors_test ] = learn_RBF_SGD( X_train, y_train, mdl_params, iterations,visualize, X_test,y_test, eta_c, sgd_errors); 
-    else
-        error('The train function you gave: %s does not exist', train_func_name);
-    end
-    mdl_current = mdl_func( mdl_params );
-    error_mdl_new_on_cv = compute_Hf_sq_error(X_cv,y_cv, mdl_current, mdl_current.lambda );
-    if error_mdl_new_on_cv < error_best_mdl_on_cv
-        best_iteration_mdl = mdl_current;
-        error_best_mdl_on_cv = error_mdl_new_on_cv;
-        best_train = errors_train;
-        best_test = errors_test;
-        c_best = c_init;
-        t_best = t_init;
-    end   
+if gpu_on   
+    X_train = gpuArray(X_train);
+    y_train = gpuArray(y_train);
+    X_cv = gpuArray(X_cv);
+    y_cv = gpuArray(y_cv);
+    X_test = gpuArray(X_test);
+    y_test = gpuArray(y_test);
 end
+if rbf_as_initilization
+    %% RBF with linear algebra (LA)
+    K = center;
+    c_init = (1 + 1)*rand(K,D_out) - 1;
+    t_init = datasample(X_train', K, 'Replace', false)'; % (D x N)
+    if c_init_normalized
+        c_init = normc(c_init);
+    end
+    if gpu_on
+        c_init = gpuArray(c_init);
+        t_init = gpuArray(t_init);
+    end
+    rbf_mdl = RBF(c_init,t_init,gau_precision,lambda);
+    rbf_mdl = learn_RBF_linear_algebra( X_train, y_train, rbf_mdl);
+    %% train HBF1
+    c_init = rbf_mdl.c;
+    t_init = rbf_mdl.t;
+    mdl = HBF1(c_init,t_init,gau_precision,lambda);
+    [ best_iteration_mdl, best_train, best_test ] = learn_HBF1_SGD( X_train, y_train, mdl, iterations,visualize, X_test,y_test, eta_c,eta_t, sgd_errors);
+else
+    error_best_mdl_on_cv = inf;
+    best_iteration_mdl = -1;
+    %mdl_func = str2func(mdl_func_name)
+    for initialization_index=1:num_inits
+        fprintf('initialization_index = %d\n\n', initialization_index);
+        K = center;
+        c_init = (1 + 1)*rand(K,D_out) - 1;
+        t_init = datasample(X_train', K, 'Replace', false)'; % (D x N)
+        if c_init_normalized
+            c_init = normc(c_init);
+        end
+        if gpu_on
+            c_init = gpuArray(c_init);
+            t_init = gpuArray(t_init);
+        end
+        if strcmp( train_func_name, 'learn_HBF1_SGD')
+            mdl = HBF1(c_init,t_init,gau_precision,lambda);
+            [ mdl, errors_train, errors_test ] = learn_HBF1_SGD( X_train, y_train, mdl, iterations,visualize, X_test,y_test, eta_c,eta_t, sgd_errors);
+        elseif strcmp( train_func_name, 'learn_RBF_SGD')
+            mdl = RBF(c_init,t_init,gau_precision,lambda);
+            [ mdl, errors_train, errors_test ] = learn_RBF_SGD( X_train, y_train, mdl, iterations,visualize, X_test,y_test, eta_c, sgd_errors); 
+        else
+            error('The train function you gave: %s does not exist', train_func_name);
+        end
+        error_mdl_new_on_cv = compute_Hf_sq_error(X_cv,y_cv, mdl, mdl.lambda );
+        if error_mdl_new_on_cv < error_best_mdl_on_cv
+            best_iteration_mdl = mdl;
+            error_best_mdl_on_cv = error_mdl_new_on_cv;
+            best_train = errors_train;
+            best_test = errors_test;
+            c_best = c_init;
+            t_best = t_init;
+        end
+    end
+    %% RBF with linear algebra (LA)
+    rbf_mdl = RBF(c_best,t_best,gau_precision, best_iteration_mdl.lambda);
+    rbf_mdl = learn_RBF_linear_algebra( X_train, y_train, rbf_mdl);
+else
+    %% RBF with linear algebra (LA)
+    K = center;
+    c_init = (1 + 1)*rand(K,D_out) - 1;
+    t_init = datasample(X_train', K, 'Replace', false)'; % (D x N)
+    if c_init_normalized
+        c_init = normc(c_init);
+    end
+    if gpu_on
+        c_init = gpuArray(c_init);
+        t_init = gpuArray(t_init);
+    end
+    rbf_mdl = RBF(c_init,t_init,gau_precision,lambda);
+    rbf_mdl = learn_RBF_linear_algebra( X_train, y_train, rbf_mdl);
+    %% train HBF1
+    c_init = rbf_mdl.c;
+    t_init = rbf_mdl.t;
+    mdl = HBF1(c_init,t_init,gau_precision,lambda);
+    [ best_iteration_mdl, best_train, best_test ] = learn_HBF1_SGD( X_train, y_train, mdl, iterations,visualize, X_test,y_test, eta_c,eta_t, sgd_errors);
+end
+%% Errors of models
+test_error_RBF = compute_Hf_sq_error(X_test,y_test, rbf_mdl, rbf_mdl.lambda )
+train_error_RBF = compute_Hf_sq_error(X_train,y_train, rbf_mdl, rbf_mdl.lambda )
 train_error_HBF1 = compute_Hf_sq_error(X_train,y_train, best_iteration_mdl, best_iteration_mdl.lambda )
 test_error_HBF1 = compute_Hf_sq_error(X_test,y_test, best_iteration_mdl, best_iteration_mdl.lambda )
-%% write errors during iterations
+best_iteration_mdl = best_iteration_mdl.gather();
+rbf_mdl = rbf_mdl.gather();
+%% save everything/write errors during iterations
+rbf_mdl = rbf_mdl.gather();
+best_iteration_mdl = best_iteration_mdl.gather()
 vname=@(x) inputname(1);
 error_iterations_file_name = sprintf('test_error_vs_iterations%d',task_id);
 path_error_iterations = sprintf('%s%s',results_path,error_iterations_file_name)
-%% RBF with linear algebra (LA)
-rbf_mdl_params = RBF_parameters(c_best,t_best,gau_precision,best_iteration_mdl.lambda);
-rbf_mdl_params = learn_RBF_linear_algebra( X_train, y_train, rbf_mdl_params);
-rbf_mdl = RBF(rbf_mdl_params);
-test_error_RBF = compute_Hf_sq_error(X_test,y_test, rbf_mdl, rbf_mdl_params.lambda )
-train_error_RBF = compute_Hf_sq_error(X_train,y_train, rbf_mdl, rbf_mdl_params.lambda )
 save(path_error_iterations, vname(best_train),vname(best_test), vname(center), vname(iterations), vname(eta_c), vname(eta_t), vname(best_iteration_mdl), vname(rbf_mdl) );
 %% write results to file
 result_file_name = sprintf('results_om_id%d.m',task_id);
