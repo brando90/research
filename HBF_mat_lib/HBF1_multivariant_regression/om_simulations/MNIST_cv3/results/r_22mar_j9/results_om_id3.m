69c2d16050fdbe827ba702fe82f53348eb15b8c9
Brando <brando90@mit.edu>
2016-03-25 00:21:14 -0400

    test gpu HSig

---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/MNIST_cv3/results/r_22mar_j9/results_om_id3.m
@@ -0,0 +1,6 @@
+task_id=3;
+center=56;
+test_error_HBF1=5.354737e+01;
+train_error_HBF1=5.351198e+01;
+test_error_RBF=1.083792e+01;
+train_error_RBF=1.109639e+01;
---
+++ HBF_mat_lib/HBF1_multivariant_regression/om_simulations/MNIST_cv3/results/r_22mar_j9/results_om_id4.m
@@ -0,0 +1,6 @@
+task_id=4;
+center=84;
+test_error_HBF1=5.628692e+01;
+train_error_HBF1=5.631134e+01;
+test_error_RBF=7.193565e+00;
+train_error_RBF=7.387539e+00;
